aed4757c973e4df84327fc24d142d7f885f72e03
Ruan <ruannawe95@outlook.com>
2024-10-08 16:27:07 -0300

    Create string.h

---
+++ include/calgostr/string.h
@@ -0,0 +1,9 @@
+#ifndef CALGOSTR_STRING_H
+#define CALGOSTR_STRING_H
+
+#include <stddef.h>  // For size_t
+#include <stdbool.h> // For bool type
+
+char* trim(const char* str);
+
+#endif // CALGOSTR_STRING_H
