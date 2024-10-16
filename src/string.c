9d1da62e6426699a818e926c38e9e3eba9908e40
Ruan <ruannawe95@outlook.com>
2024-10-16 15:12:32 +0000

    update string.c

--- src/string.c
+++ src/string.c
@@ -2,30 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 
 char* trim(const char* str)
 {
     if (str == NULL) return NULL;
 
-    size_t len = strlen(str);
-    size_t start = 0;
-    size_t end = len;
+    size_t str_index = 0;
+    size_t str_len = strlen(str);
+    bool can_be_space = false;
+    char* str_temp = calloc(str_len + 1, sizeof(char));
 
-    while (start < len && isspace((unsigned char)str[start])) start++;
-    if (start == len) {
-        char* result = calloc(1, sizeof(char));
-        if (result != NULL) result[0] = '\0';
-        return result;
+    for (size_t i = 0; i < str_len; i++)
+    {
+        if (!isspace((unsigned char)str[i]))
+        {
+            str_temp[str_index++] = str[i];
+            can_be_space = true;
+        }
+        else if (isspace((unsigned char)str[i]) && can_be_space)
+        {
+            str_temp[str_index++] = ' ';
+            can_be_space = false;
+        }
     }
-    end = len - 1;
-    while (end > start && isspace((unsigned char)str[end])) end--;
-    
-    size_t trimmed_length = end - start + 1;
-    char* result = calloc(trimmed_length + 1, sizeof(char));
-    if (result == NULL) return NULL;
-    
-    strncpy(result, str + start, trimmed_length);
-    result[trimmed_length] = '\0';
-    
+
+    printf("str_index: %ld\n", str_index);
+
+    if (str_index > 0 && isspace((unsigned char)str_temp[str_index - 1])) str_index--;
+
+    char* result = calloc(str_index + 1, sizeof(char));
+
+    printf("str_index: %ld\n", str_index);
+    printf("str_temp: %ld\n", strlen(str_temp));
+    printf("str_temp: %sFIM\n", str_temp);
+
+    strncpy(result, str_temp, str_index);
+
+    printf("result: %ld\n", strlen(result));
+    printf("result: %sFIM\n", result);
+
+    free(str_temp);
+
     return result;
 }
